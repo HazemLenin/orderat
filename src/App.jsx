e13bce1b23fbf281ed9ce1470080082e7c21167a
Rawan Mohamed Kamel <rawanmohamedkamel8@gmail.com>
2023-09-21 18:09:49 +0300

    menu structure & rawadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VisaModal from "./pages/VisaModal";
+import Menu from "./pages/Menu";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes></Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      {/* <VisaModal/> */}
+      <Routes>
+        <Route path="/restaurants/:id" element={<Menu/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
