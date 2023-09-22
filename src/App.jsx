813b5207b8055933cc6e8d3665151656febaadb8
OmarKing015 <66084757+OmarKing015@users.noreply.github.com>
2023-09-22 18:13:44 +0200

    Cart logic initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import { createContext } from "react";
+import { useEffect } from "react";
+import { data } from "autoprefixer";
+import { CartContext } from "./components/cart";
 
 function App() {
-	return (
-		<Router>
-			<Navbar/>
-			<Routes></Routes>
-	
-		
-			</Router>
-	);
+  const cartlogic = useContext(CartContext)
+  
+  return (
+    
+      <Router>
+        <Navbar />
+        <Routes></Routes>
+      </Router>
+  
+  );
 }
 
 export default App;
