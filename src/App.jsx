5a1c6b024281f320734e7d317c9b0f904f7ebcb5
Hazem Lenin <hazemlenin@gmail.com>
2023-09-22 18:06:28 +0300

    code cleaning 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
-import './index.css'
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
-import Map from './Pages/Map';
-import { createContext, useState } from 'react';
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Map from "./Pages/Map";
+import { createContext, useState } from "react";
 import Menu from "./pages/Menu";
-
-export const AddressContext=createContext([
-  {
-    lat: 31.25214048726485,
-	  lng:  30.009838471967534,
-    isMap: true
-  },
-  () => {}
-]);
-
-
+import AddressContext from "./contexts/AddressContext";
 
 function App() {
-const [address , setAddress] = useState({
-  lat: 31.25214048726485,
-	lng:  30.009838471967534,
-  isMap: true
-});
+	const [address, setAddress] = useState({
+		lat: 31.25214048726485,
+		lng: 30.009838471967534,
+		isMap: true,
+	});
 
 	return (
-		<AddressContext.Provider value={ [address , setAddress]}>
-<BrowserRouter>
-<Routes>
-  <Route path="/address" element={<Map/>} />
-    <Route path="/restaurants/:id" element={<Menu/>}/>
-</Routes>
-</BrowserRouter>
-
-</AddressContext.Provider>
- 	);
- }
+		<AddressContext.Provider value={[address, setAddress]}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/address" element={<Map />} />
+					<Route path="/restaurants/:id" element={<Menu />} />
+				</Routes>
+			</BrowserRouter>
+		</AddressContext.Provider>
+	);
+}
 
 export default App;
--- src/components/DraggableMarker.jsx
+++ src/components/DraggableMarker.jsx
@@ -1,52 +1,40 @@
-import {  useCallback,useContext,useMemo, useRef , useState} from "react";
-import { Marker , Popup } from "react-leaflet";
-import './map.css';
+import { useCallback, useContext, useMemo, useRef, useState } from "react";
+import { Marker, Popup } from "react-leaflet";
+import "./map.css";
 import { AddressContext } from "../App";
 
-const center = {
-	lat: 31.25214048726485,
-	lng:  30.009838471967534,
-  } 
-  
-  function DraggableMarker() {
-    const [address , setAddress] = useContext(AddressContext)
-	const [draggable, setDraggable] = useState(false)
-	const markerRef = useRef(null)
+function DraggableMarker() {
+	const [address, setAddress] = useContext(AddressContext);
+	const [draggable, setDraggable] = useState(false);
+	const markerRef = useRef(null);
 	const eventHandlers = useMemo(
-	  () => ({
-		dragend() {
-		  const marker = markerRef.current
-		  if (marker != null) {
-			let newAdress = marker.getLatLng();
-			newAdress.isMap = true;
-			setAddress(newAdress)
-			console.log(address)
-
-		  }
-		},
-	  }),
-	  [],
-	)
-	
-	console.log(address);
-	
+		() => ({
+			dragend() {
+				const marker = markerRef.current;
+				if (marker != null) {
+					let newAdress = marker.getLatLng();
+					newAdress.isMap = true;
+					setAddress(newAdress);
+				}
+			},
+		}),
+		[]
+	);
 	const toggleDraggable = useCallback(() => {
-	  setDraggable((d) => !d)
-	}, [])
+		setDraggable((d) => !d);
+	}, []);
 
 	return (
-	  <Marker
-		draggable
-		eventHandlers={eventHandlers}
-		position={address}
-		ref={markerRef}>
-		<Popup minWidth={90}>
-		  <span onClick={toggleDraggable}>
-			{draggable}
-		  </span>
-		</Popup>
-	  </Marker>
-	)
-  }
-  export default DraggableMarker;
-
+		<Marker
+			draggable
+			eventHandlers={eventHandlers}
+			position={address}
+			ref={markerRef}
+		>
+			<Popup minWidth={90}>
+				<span onClick={toggleDraggable}>{draggable}</span>
+			</Popup>
+		</Marker>
+	);
+}
+export default DraggableMarker;
