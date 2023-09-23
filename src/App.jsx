b57dc4c60fd6d2a4c5b406c0ce17c4f22e9df911
tassneem omar <tassneemomar880@gmail.com>
2023-09-23 16:47:52 +0300

    chechout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import "../src/index.css";
+import Checkout from "./pages/Checkout";
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes></Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+
+      <Routes>
+        < Route path="/Checkout" element={<Checkout/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+export default App; 
