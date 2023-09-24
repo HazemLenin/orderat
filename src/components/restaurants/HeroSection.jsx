51630ec7eddca220d901392ddf85ab45bfa6c3bd
Hazem Lenin <hazemlenin@gmail.com>
2023-09-24 05:09:11 +0300

    bug fixes 6

--- src/components/restaurants/HeroSection.jsx
+++ src/components/restaurants/HeroSection.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function HeroSection() {
 	return (
-		<div className="relative  bg-no-repeat opacity-80 bg-cover bg-center w-full flex">
+		<div className="relative  bg-no-repeat opacity-80 bg-cover bg-center w-full flex md:h-auto h-60">
 			<div className=" absolute top-0 left-0 w-full h-full bg-red-600/80 flex flex-col gap-10 justify-center items-center text-white">
 				<h1 className="text-9xl font-['Kaushan_Script']">Quicky</h1>
-				<h3 className="text-4xl">
+				<h3 className="hidden md:block text-4xl">
 					Quicky: Fast, Fresh, and Delivered in a Dash!
 				</h3>
 			</div>
